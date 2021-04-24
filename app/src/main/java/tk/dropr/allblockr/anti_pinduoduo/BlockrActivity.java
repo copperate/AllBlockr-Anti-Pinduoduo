83b2ab281514f914fbe2c104d64e54cf34e7901a
Copyerder <44644190+Copyerder@users.noreply.github.com>
2021-04-24 23:05:31 +0800

    拼多多猎马人

---
+++ app/src/main/java/tk/dropr/allblockr/anti_pinduoduo/BlockrActivity.java
@@ -0,0 +1,13 @@
+package tk.dropr.allblockr.anti_pinduoduo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlockrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blockr);
+    }
+}
